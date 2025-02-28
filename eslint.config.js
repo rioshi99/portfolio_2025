9f787152c7443b92b9cc57778665b21d65747b26
Shiori Sugahara <sugaharawebwork@gmail.com>
2025-03-01 00:37:22 +0900

    change setting [Laptop]

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import jsxA11y from 'eslint-plugin-jsx-a11y';
 export default [
 	...eslintPluginAstro.configs.recommended,
 	{
-		files: ['**/*.{js,astro}'],
+		files: ['**/*.{js,jsx,ts,tsx,astro}'],
 		plugins: {
 			'jsx-a11y': jsxA11y, // JSXのアクセシビリティチェックを追加
 		},
