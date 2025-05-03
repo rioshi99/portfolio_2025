71675bc65bcc3f243322a331cde4e3d43e6b8847
Shiori Sugahara <sugaharawebwork@gmail.com>
2025-05-03 17:14:08 +0900

    eslint調整[deskTop]

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,21 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
+import eslintRecommended from '@eslint/js';
 
 export default [
+	eslintRecommended.configs.recommended,
 	...eslintPluginAstro.configs.recommended,
 	{
-		files: ['**/*.{js,jsx,ts,tsx,astro}'],
+		files: ['**/*.{js,astro}'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			globals: {
+				console: 'readonly',
+				document: 'readonly',
+				window: 'readonly',
+			},
+		},
 		plugins: {
 			'jsx-a11y': jsxA11y, // JSXのアクセシビリティチェックを追加
 		},
